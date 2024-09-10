692c47ef42afa939435e2ba9533187ba2328a038
Julien Verneaut <jverneaut@gmail.com>
2024-09-10 23:46:39 +0200

    Setup TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './web/app/themes/theme/assets/**/*.js',
+    './web/app/themes/theme/views/**/*.twig',
+    './web/app/themes/theme/blocks/**/*.js',
+    './web/app/themes/theme/blocks/**/*.php',
+    './web/app/themes/theme/blocks/**/*.twig',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -147,6 +147,7 @@ Encore
 
   // enables Sass/SCSS support
   .enableSassLoader()
+  .enablePostCssLoader()
   // uncomment if you use TypeScript
   //.enableTypeScriptLoader()
 
