e166270c04667b95555c1d4854ca94a21b4af8b9
Julien Verneaut <jverneaut@gmail.com>
2024-09-10 23:37:30 +0200

    Add custom Blocks bundling webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,86 @@
 const Encore = require('@symfony/webpack-encore');
+const glob = require('glob');
+const fs = require('fs');
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const DependencyExtractionWebpackPlugin = require('@wordpress/dependency-extraction-webpack-plugin');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
   Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
+
+const blocks = glob
+  .sync('./web/app/themes/theme/blocks/**/block.json')
+  .reduce((acc, curr) => {
+    const json = JSON.parse(
+      fs.readFileSync(path.join(__dirname, curr), 'utf8'),
+    );
+
+    const base = curr.replace('block.json', '');
+
+    const properties = [
+      'editorScript',
+      'editorStyle',
+      'style',
+      'render',
+      'viewScript',
+    ];
+
+    const block = {
+      entries: {},
+      files: [
+        {
+          from: path.join(__dirname, curr),
+          to: `blocks/${json.name}/block.json`,
+          transform(content) {
+            const blockJSON = JSON.parse(content);
+
+            properties.forEach((property) => {
+              if (blockJSON[property] && blockJSON[property].includes('scss')) {
+                blockJSON[property] = `file:${blockJSON[property]
+                  .replace('file:', '')
+                  .replace('scss', 'css')}`;
+              }
+            });
+
+            return JSON.stringify(blockJSON);
+          },
+        },
+      ],
+    };
+
+    properties.forEach((property) => {
+      if (json[property]) {
+        if (
+          json[property].indexOf('.php') > -1 ||
+          json[property].indexOf('.twig') > -1
+        ) {
+          block.files.push({
+            from: path.join(
+              __dirname,
+              base,
+              json[property].replace('file:', ''),
+            ),
+            to: path.join(
+              `blocks/${json.name}`,
+              json[property].replace('file:', ''),
+            ),
+          });
+        } else {
+          block.entries[
+            `blocks/${json.name}/${json[property].split('./')[1].split('.')[0]}`
+          ] = path.join(__dirname, base, json[property].replace('file:', ''));
+        }
+      }
+    });
+
+    return {
+      ...acc,
+      [json.name]: block,
+    };
+  }, {});
 
 Encore
   // directory where compiled assets will be stored
@@ -22,15 +98,29 @@ Encore
    */
   .addEntry('app', './web/app/themes/theme/assets/js/app.js')
 
+  // Add blocks entries
+  .addEntries(
+    Object.values(blocks)
+      .map((block) => block.entries)
+      .reduce(
+        (acc, curr) => ({
+          ...acc,
+          ...curr,
+        }),
+        {},
+      ),
+  )
+
   // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
   // .enableStimulusBridge('./assets/controllers.json')
 
   // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
-  .splitEntryChunks()
+  // .splitEntryChunks()
 
   // will require an extra script tag for runtime.js
   // but, you probably want this, unless you're building a single-page app
-  .enableSingleRuntimeChunk()
+  // .enableSingleRuntimeChunk()
+  .disableSingleRuntimeChunk()
 
   /*
    * FEATURE CONFIG
@@ -43,7 +133,7 @@ Encore
   .enableBuildNotifications()
   .enableSourceMaps(!Encore.isProduction())
   // enables hashed filenames (e.g. app.abc123.css)
-  .enableVersioning(Encore.isProduction())
+  // .enableVersioning(Encore.isProduction())
 
   .configureBabel((config) => {
     config.plugins.push('@babel/plugin-transform-class-properties');
@@ -56,13 +146,24 @@ Encore
   })
 
   // enables Sass/SCSS support
-  .enableSassLoader();
+  .enableSassLoader()
+  // uncomment if you use TypeScript
+  //.enableTypeScriptLoader()
 
-// uncomment if you use TypeScript
-//.enableTypeScriptLoader()
+  // uncomment if you use React
+  .enableReactPreset()
 
-// uncomment if you use React
-//.enableReactPreset()
+  .addPlugin(
+    Object.keys(blocks).length
+      ? new CopyPlugin({
+          patterns: Object.values(blocks)
+            .map((block) => block.files)
+            .flat(),
+        })
+      : { apply: () => {} },
+  )
+
+  .addPlugin(new DependencyExtractionWebpackPlugin());
 
 // uncomment to get integrity="..." attributes on your script & link tags
 // requires WebpackEncoreBundle 1.4 or higher
