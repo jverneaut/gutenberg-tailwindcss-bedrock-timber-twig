09ade37be27cc87e669514b25fdff275e1ac93ed
Julien Verneaut <jverneaut@gmail.com>
2025-03-25 11:45:55 +0100

    Format files

--- web/app/themes/theme/functions.php
+++ web/app/themes/theme/functions.php
@@ -3,7 +3,6 @@
 require_once __DIR__ . '/../../../../vendor/autoload.php';
 
 use App\Bootstrap;
-
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
