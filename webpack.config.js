aded7a9c613e60818b703c22ecd472a61ccefc27
Julien Verneaut <jverneaut@gmail.com>
2025-04-08 15:31:39 +0200

    Use @jverneaut/gutenberg-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,90 +1,18 @@
 require('dotenv').config();
 
 const Encore = require('@symfony/webpack-encore');
-const glob = require('glob');
-const fs = require('fs');
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
-const DependencyExtractionWebpackPlugin = require('@wordpress/dependency-extraction-webpack-plugin');
+
 const chokidar = require('chokidar');
 const CleanTerminalPlugin = require('clean-terminal-webpack-plugin');
+
+const GutenbergWebpackPlugin =
+  require('@jverneaut/gutenberg-webpack-plugin').default;
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
   Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
-
-const blocks = glob
-  .sync('./web/app/themes/theme/blocks/**/block.json')
-  .reduce((acc, curr) => {
-    const json = JSON.parse(
-      fs.readFileSync(path.join(__dirname, curr), 'utf8')
-    );
-
-    const base = curr.replace('block.json', '');
-
-    const properties = [
-      'editorScript',
-      'editorStyle',
-      'style',
-      'render',
-      'viewScript',
-    ];
-
-    const block = {
-      entries: {},
-      files: [
-        {
-          from: path.join(__dirname, curr),
-          to: `blocks/${json.name}/block.json`,
-          transform(content) {
-            const blockJSON = JSON.parse(content);
-
-            properties.forEach((property) => {
-              if (blockJSON[property] && blockJSON[property].includes('scss')) {
-                blockJSON[property] = `file:${blockJSON[property]
-                  .replace('file:', '')
-                  .replace('scss', 'css')}`;
-              }
-            });
-
-            return JSON.stringify(blockJSON);
-          },
-        },
-      ],
-    };
-
-    properties.forEach((property) => {
-      if (json[property]) {
-        if (
-          json[property].indexOf('.php') > -1 ||
-          json[property].indexOf('.twig') > -1
-        ) {
-          block.files.push({
-            from: path.join(
-              __dirname,
-              base,
-              json[property].replace('file:', '')
-            ),
-            to: path.join(
-              `blocks/${json.name}`,
-              json[property].replace('file:', '')
-            ),
-          });
-        } else {
-          block.entries[
-            `blocks/${json.name}/${json[property].split('./')[1].split('.')[0]}`
-          ] = path.join(__dirname, base, json[property].replace('file:', ''));
-        }
-      }
-    });
-
-    return {
-      ...acc,
-      [json.name]: block,
-    };
-  }, {});
 
 Encore
   // directory where compiled assets will be stored
@@ -101,19 +29,7 @@ Encore
    * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
    */
   .addEntry('app', './web/app/themes/theme/assets/js/app.js')
-
-  // Add blocks entries
-  .addEntries(
-    Object.values(blocks)
-      .map((block) => block.entries)
-      .reduce(
-        (acc, curr) => ({
-          ...acc,
-          ...curr,
-        }),
-        {}
-      )
-  )
+  .addPlugin(new GutenbergWebpackPlugin('./web/app/themes/theme/blocks'))
 
   // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
   // .enableStimulusBridge('./assets/controllers.json')
@@ -157,18 +73,6 @@ Encore
 
   // uncomment if you use React
   .enableReactPreset()
-
-  .addPlugin(
-    Object.keys(blocks).length
-      ? new CopyPlugin({
-          patterns: Object.values(blocks)
-            .map((block) => block.files)
-            .flat(),
-        })
-      : { apply: () => {} }
-  )
-
-  .addPlugin(new DependencyExtractionWebpackPlugin())
 
   .configureDevServerOptions((options) => {
     options.proxy = {
